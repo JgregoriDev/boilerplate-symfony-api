ade4881b3aa1c134a568f3ec74c77a6ac65d74a7
Josep Gregori <josepgb13@gmail.com>
2023-08-09 07:43:36 -0400

    nelmio packages family and fostrestbundle installed

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
